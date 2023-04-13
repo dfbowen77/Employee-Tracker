268499df8bdba5f4bacc5fdffe0d5f36fb723111
Dan Bowen <danbowen2677@gmail.com>
2023-04-13 12:50:49 -0400

    fixed simple sql errors

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
+SET FOREIGN_KEY_CHECKS=0;
+
 INSERT INTO department (department_name)
 VALUES ("Information Technology"),
        ("Research and Development"),
@@ -6,7 +8,7 @@ VALUES ("Information Technology"),
        ("Software Products"),
        ("Legal"),
        ("Marketing"),
-       ("Network Operations")
+       ("Network Operations"),
        ("Executive Management");
 
 INSERT INTO role (title, salary, department_id)
@@ -26,26 +28,26 @@ VALUES ("Senior Software Developer", 150000, 5),
        ("Director of Human Resources", 145000, 3),
        ("Administrative Assistant", 50000, 3),
        ("Attorney", 285000, 6),
-       ("Paralegal", 75000, 6)
+       ("Paralegal", 75000, 6),
        ("Chief Executive Officer", 575000, 9);  
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) 
-VALUES ("Stephanie","Portman",15, 2)
-       ("Symphony","Keller",14,18)
-       ("Renee","Baldwin",9,4)
-       ("Jack","Miller",8,18)
-       ("Simon","Lind",7,4)
-       ("Doreen","Beryl",17,7)
-       ("Colby","Sheen",16,)
-       ("Cindy","Caden",13,1)
-       ("Mason","Hunter",12,1)
-       ("Ronan","Pierce",11,1)
-       ("Edna","Hale",10,1)
-       ("Luis","Reeves",6,5)
-       ("Christopher","O'Ryan",5,18)
-       ("Hope","Wilkerson",4,18)
-       ("Charlie","Hogan",3,15)
-       ("Elinor","Mueller",2,1)
-       ("Yusuf","Lara",1,18)
-       ("Isaac","Dalton",18,)
+VALUES ("Stephanie","Portman",15, 2),
+       ("Symphony","Keller",14,18),
+       ("Renee","Baldwin",9,4),
+       ("Jack","Miller",8,18),
+       ("Simon","Lind",7,4),
+       ("Doreen","Beryl",17,7),
+       ("Colby","Sheen",16,18),
+       ("Cindy","Caden",13,1),
+       ("Mason","Hunter",12,1),
+       ("Ronan","Pierce",11,1),
+       ("Edna","Hale",10,1),
+       ("Luis","Reeves",6,5),
+       ("Christopher","O'Ryan",5,18),
+       ("Hope","Wilkerson",4,18),
+       ("Charlie","Hogan",3,15),
+       ("Elinor","Mueller",2,1),
+       ("Yusuf","Lara",1,18),
+       ("Isaac","Dalton",18,18)
        ;    
