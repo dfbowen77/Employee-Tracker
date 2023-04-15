24c0ad7c60ad6811d8470949c9c2b10e1ce1d036
Dan Bowen <danbowen2677@gmail.com>
2023-04-15 10:10:28 -0400

    set-up the database connection

--- server.js
+++ server.js
@@ -10,9 +10,20 @@ const cTable = require('console.table')
 // Imports the asciiart-logo package in order to spice up the command line interface. 
 const logo = require('asciiart-logo')
 
-// Here is where the questions for the user to answer are created.
+// Connects to the mysql database. 
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // TODO: Add MySQL password here
+    password: '12345',
+    database: 'employeetracker_db'
+  },
+  console.log(`Connected to the employeetracker_db database.`)
+);
 
-// TODO: Research whether I need to break these questions up into different arrays (e.g. one array for adding a role and a different array for the main prompt of questions).  
+// Here is where the questions for the user to answer are created.
 const initialQuestion = [
     {
       type: 'list',
